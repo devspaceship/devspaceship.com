9121ba3e376e5bea632e2c78fbb6ca60836c23ae
Thomas Saint-Gérand <devspaceship@gmail.com>
2020-08-16 17:04:06 +0200

    Added favicon

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -1,5 +1,12 @@
+import HomeIcon from '@material-ui/icons/Home';
+import styles from '../styles/Layout.module.scss';
+
 import { FC } from 'react';
 
-const NavBar: FC = () => <h1>Nav Bar</h1>;
+const NavBar: FC = () => (
+  <div className={styles.navbar}>
+    <HomeIcon fontSize="large" />
+  </div>
+);
 
 export default NavBar;
