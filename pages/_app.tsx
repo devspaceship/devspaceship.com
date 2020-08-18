22368f8d009f3866420d2889e5e5bf4fb0f6dea1
Thomas Saint-Gérand <devspaceship@gmail.com>
2020-08-18 20:11:14 +0200

    Changed katex css import in app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,15 +4,10 @@ import { FC } from 'react';
 import Layout from '../components/Layout';
 
 import 'prismjs/themes/prism-okaidia.css';
+import 'katex/dist/katex.min.css';
 
 const MyApp: FC<AppProps> = ({ Component, pageProps }) => (
   <Layout>
-    <link
-      rel="stylesheet"
-      href="https://cdn.jsdelivr.net/npm/katex@0.12.0/dist/katex.min.css"
-      integrity="sha384-AfEj0r4/OFrOo5t7NnNe46zW/tFgW6x/bCJG8FqQCEo3+Aro6EYUG4+cU+KJWu/X"
-      crossOrigin="anonymous"
-    />
     <Component {...pageProps} />
   </Layout>
 );
