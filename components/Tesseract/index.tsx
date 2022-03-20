411339ac1abe0fec7aedc50015beaf38acaac765
Thomas Saint-Gérand <devspaceship@gmail.com>
2022-03-20 20:12:09 +0100

    Installed React Three Fiber

---
+++ components/Tesseract/index.tsx
@@ -0,0 +1,13 @@
+import Display from "./display";
+import Control from "./control";
+
+const Tesseract = () => {
+  return (
+    <>
+      <Display />
+      <Control />
+    </>
+  );
+};
+
+export default Tesseract;
