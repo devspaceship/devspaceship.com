67ef36d906ec10ae499422138cde8903e58f20f6
Thomas Saint-Gérand <devspaceship@gmail.com>
2022-04-05 19:03:47 +0200

    Updating index page

--- pages/index.tsx
+++ pages/index.tsx
@@ -25,16 +25,11 @@ const Home: FC = () => (
       <h1 className={styles.title}>Science Decoder</h1>
       <p className={styles.description}>
         Hello, my name is Thomas Saint-Gérand. I am a French developer currently
-        living, studying and working in Amsterdam, The Netherlands.
+        living in Amsterdam, The Netherlands.
         <br />
         This website is intented to be a repository of stuff that I find cool,
         interesting, visually pleasing or all of these. I try to answer specific
         questions with detailed examples.
-      </p>
-      <p>
-        This website is still under construction.
-        <br />
-        It does not contain the posts yet
       </p>
     </main>
   </>
