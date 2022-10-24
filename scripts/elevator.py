c6a03b732b0231089ff9e1c1039423071559509e
Thomas Saint-Gérand <devspaceship@gmail.com>
2022-10-24 23:07:59 +0200

    [elevator] Changed radius to km

--- scripts/elevator.py
+++ scripts/elevator.py
@@ -10,7 +10,7 @@ STATIC_DIR = PARENT_DIR / "public" / "static" / "posts" / "elevator"
 def main():
     raw_df = pd.read_csv(DATA_DIR / "PREM500.csv")
     df = pd.DataFrame()
-    df["radius"] = raw_df["radius(m)"]
+    df["radius"] = raw_df["radius(m)"] / 1e3
     df["density"] = raw_df["density(kg/m^3)"] / 1e3
     df.to_csv(STATIC_DIR / "density.csv", index=False)
 
