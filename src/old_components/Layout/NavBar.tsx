03a97ab9e5e068af0940e76055e88846388929a5
Thomas Saint-Gérand <devspaceship@gmail.com>
2023-05-17 06:52:48 +0200

    renamed components dir

--- src/old_components/Layout/NavBar.tsx
+++ src/old_components/Layout/NavBar.tsx
@@ -1,18 +1,18 @@
-import Link from "next/link";
+// import Link from "next/link";
 import styles from "../../styles/Layout.module.scss";
 
 import { FC } from "react";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faGraduationCap, faHouse } from "@fortawesome/free-solid-svg-icons";
+// import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+// import { faGraduationCap, faHouse } from "@fortawesome/free-solid-svg-icons";
 
 const NavBar: FC = () => (
   <nav className={styles.navbar}>
-    <Link href="/" passHref>
+    {/* <Link href="/" passHref>
       <FontAwesomeIcon icon={faHouse} size="2x" />
     </Link>
     <Link href="/posts" passHref>
       <FontAwesomeIcon icon={faGraduationCap} size="2x" />
-    </Link>
+    </Link> */}
     {/* <Link href="/resume" passHref>
     </Link> */}
   </nav>
