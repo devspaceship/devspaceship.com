c1d7f73faa86bddac160ba43c0d71eb4a504d18b
Thomas Saint-Gérand <devspaceship@gmail.com>
2023-05-23 20:50:40 +0200

    added ga

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import NavBar from "@/components/NavBar";
+import Script from "next/script";
 config.autoAddCss = false;
 
 const inter = Inter({ subsets: ["latin"] });
@@ -25,6 +26,19 @@ export default function RootLayout({
         <main className="container mx-auto flex h-full flex-col items-center justify-start space-y-12 text-center">
           {children}
         </main>
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-VJTBJP5KDG"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){window.dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-VJTBJP5KDG');
+        `}
+        </Script>
       </body>
     </html>
   );
