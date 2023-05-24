947b296ccee788109a1711c3d8ee49804801cb44
Thomas Saint-Gérand <devspaceship@gmail.com>
2023-05-24 22:00:57 +0200

    metadata for front page

---
+++ src/app/sitemap.ts
@@ -0,0 +1,17 @@
+import { MetadataRoute } from "next";
+
+const routes = [
+  "",
+  "/posts",
+  "/posts/gridworld",
+  "/posts/tesseract",
+  "/posts/bluesky",
+  "/posts/elevator",
+];
+
+export default function sitemap(): MetadataRoute.Sitemap {
+  return routes.map((route) => ({
+    url: `https://devspaceship.com${route}`,
+    lastModified: new Date(),
+  }));
+}
