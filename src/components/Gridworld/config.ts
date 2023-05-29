109390fb0302a500e04c3c7cd212c13beddca302
Thomas Saint-Gérand <devspaceship@gmail.com>
2023-05-29 10:20:00 +0200

    moved types and initial grid out of reducer file

--- src/components/Gridworld/config.ts
+++ src/components/Gridworld/config.ts
@@ -1,4 +1,26 @@
+import { CellType } from "./types";
+
 export const WIDTH = 12;
 export const HEIGHT = 8;
 export const CELL_PADDING = 0.03;
 export const CORNER_ROUNDING = "25%";
+
+const gridMapping: Record<number, CellType> = {
+  0: CellType.EMPTY,
+  1: CellType.WALL,
+  2: CellType.START,
+  3: CellType.END,
+};
+const initialGrid = [
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0],
+  [0, 0, 1, 1, 0, 1, 0, 2, 1, 0, 0, 0],
+  [0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0],
+  [0, 1, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0],
+  [0, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3],
+];
+export const INITIAL_GRID = initialGrid.map((row) =>
+  row.map((cell) => ({ type: gridMapping[cell] }))
+);
---
+++ src/components/Gridworld/types.ts
@@ -0,0 +1,41 @@
+export enum SolverType {
+  PolicyIteration,
+  ValueIteration,
+  SARSA,
+  QLearning,
+}
+
+interface GridworldConfig {
+  solver: SolverType;
+  discountRate: number;
+  threshold: number;
+  evaluationsBeforeImprovement: number;
+  iterations: number;
+  alpha: number;
+  initialExplorationCoefficient: number;
+  explorationPeriod: number;
+}
+
+export enum CellType {
+  START,
+  EMPTY,
+  WALL,
+  END,
+}
+
+enum GridDirection {
+  UP,
+  RIGHT,
+  DOWN,
+  LEFT,
+}
+
+interface CellState {
+  type: CellType;
+  direction?: GridDirection;
+}
+
+export interface GridworldState {
+  config: GridworldConfig;
+  grid: CellState[][];
+}
