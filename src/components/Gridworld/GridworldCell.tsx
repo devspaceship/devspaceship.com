20843d14449979fb466c953242cf678927091ef2
Thomas Saint-Gérand <devspaceship@gmail.com>
2023-05-29 14:06:32 +0200

    display state on grid

--- src/components/Gridworld/GridworldCell.tsx
+++ src/components/Gridworld/GridworldCell.tsx
@@ -1,6 +1,23 @@
+import { useContext } from "react";
+import { GridworldStateContext } from "./GridworldContextProvider";
 import { CELL_PADDING, CORNER_ROUNDING } from "./config";
+import { CellType } from "./types";
 
 const GridworldCell = ({ row, column }: { row: number; column: number }) => {
+  const state = useContext(GridworldStateContext);
+
+  // TODO: Remove this debug code
+  if (row == 0 && column == 0) {
+    console.log(state);
+  }
+
+  const cellTypeClasses = {
+    [CellType.EMPTY]: "fill-background-950",
+    [CellType.WALL]: "fill-primary-300",
+    [CellType.START]: "fill-secondary-500",
+    [CellType.END]: "fill-tertiary-300",
+  };
+
   return (
     <>
       <rect
@@ -9,7 +26,7 @@ const GridworldCell = ({ row, column }: { row: number; column: number }) => {
         width={1 - 2 * CELL_PADDING}
         height={1 - 2 * CELL_PADDING}
         rx={CORNER_ROUNDING}
-        className="fill-background-950"
+        className={cellTypeClasses[state.grid[row][column].type]}
       />
     </>
   );
