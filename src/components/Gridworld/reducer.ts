20cb806afe970f15547e6d0c210c718d0c7a0509
Thomas Saint-Gérand <devspaceship@gmail.com>
2023-05-29 10:20:00 +0200

    moved types and initial grid out of reducer file

--- src/components/Gridworld/reducer.ts
+++ src/components/Gridworld/reducer.ts
@@ -1,65 +1,7 @@
-enum SolverType {
-  PolicyIteration,
-  ValueIteration,
-  SARSA,
-  QLearning,
-}
+import { INITIAL_GRID } from "./config";
+import { GridworldState, SolverType } from "./types";
 
-interface GridworldConfig {
-  solver: SolverType;
-  discountRate: number;
-  threshold: number;
-  evaluationsBeforeImprovement: number;
-  iterations: number;
-  alpha: number;
-  initialExplorationCoefficient: number;
-  explorationPeriod: number;
-}
-
-enum CellType {
-  START,
-  EMPTY,
-  WALL,
-  END,
-}
-
-enum GridDirection {
-  UP,
-  RIGHT,
-  DOWN,
-  LEFT,
-}
-
-interface CellState {
-  type: CellType;
-  direction?: GridDirection;
-}
-
-interface GridworldState {
-  config: GridworldConfig;
-  grid: CellState[][];
-}
-
-const createInitialGrid = (): CellState[][] => {
-  const mapping: Record<number, CellType> = {
-    0: CellType.EMPTY,
-    1: CellType.WALL,
-    2: CellType.START,
-    3: CellType.END,
-  };
-  return [
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0],
-    [0, 0, 1, 1, 0, 1, 0, 2, 1, 0, 0, 0],
-    [0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0],
-    [0, 1, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0],
-    [0, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3],
-  ].map((row) => row.map((cell) => ({ type: mapping[cell] })));
-};
-
-const initialState: GridworldState = {
+export const initialState: GridworldState = {
   config: {
     solver: SolverType.PolicyIteration,
     discountRate: 0.97,
@@ -70,10 +12,13 @@ const initialState: GridworldState = {
     initialExplorationCoefficient: 1,
     explorationPeriod: 350,
   },
-  grid: createInitialGrid(),
+  grid: INITIAL_GRID,
 };
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-const reducer = (state: GridworldState, action: unknown): GridworldState => {
+export const reducer = (
+  state: GridworldState,
+  action: { type: string }
+): GridworldState => {
   return state;
 };
