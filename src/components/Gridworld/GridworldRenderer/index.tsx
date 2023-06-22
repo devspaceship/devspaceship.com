2c6faa37f06926a1259c643ec305a2702edfafb3
Thomas Saint-Gérand <devspaceship@gmail.com>
2023-06-22 21:31:44 +0200

    [gridworld] touch handlers

--- src/components/Gridworld/GridworldRenderer/index.tsx
+++ src/components/Gridworld/GridworldRenderer/index.tsx
@@ -1,4 +1,4 @@
-import { MouseEvent, useContext } from "react";
+import { MouseEvent, TouchEvent, useContext } from "react";
 import { GridworldDispatchContext } from "../GridworldContextProvider";
 import { GridworldActionType } from "../actions";
 import { HEIGHT, WIDTH } from "../config";
@@ -11,12 +11,27 @@ for (let row = 0; row < HEIGHT; row++) {
   }
 }
 
-const mouseEventToGridPosition = (event: MouseEvent<SVGElement>) => {
-  const { x, y, width, height } = event.currentTarget.getBoundingClientRect();
-  const { clientX, clientY } = event;
+const _getGridPosition = (
+  currentTarget: SVGElement,
+  clientX: number,
+  clientY: number
+) => {
+  const { x, y, width, height } = currentTarget.getBoundingClientRect();
   const row = Math.floor(((clientY - y) / height) * HEIGHT);
   const column = Math.floor(((clientX - x) / width) * WIDTH);
   return [row, column];
+};
+
+const mouseEventToGridPosition = (event: MouseEvent<SVGElement>) => {
+  return _getGridPosition(event.currentTarget, event.clientX, event.clientY);
+};
+
+const touchEventToGridPosition = (event: TouchEvent<SVGElement>) => {
+  return _getGridPosition(
+    event.currentTarget,
+    event.touches[0].clientX,
+    event.touches[0].clientY
+  );
 };
 
 const GridworldRenderer = () => {
@@ -48,14 +63,53 @@ const GridworldRenderer = () => {
     }
   };
 
+  const handleTouchEvent = (event: TouchEvent<SVGElement>) => {
+    event.preventDefault();
+    if (event.touches.length !== 1) {
+      return;
+    }
+    const [row, column] = touchEventToGridPosition(event);
+    switch (event.type) {
+      case "touchstart":
+        console.log("touchstart");
+        dispatch({
+          type: GridworldActionType.START_DRAWING,
+          row,
+          column,
+        });
+        break;
+      case "touchmove":
+        console.log("touchmove");
+        dispatch({
+          type: GridworldActionType.DRAW,
+          row,
+          column,
+        });
+        break;
+      case "touchend":
+        console.log("touchend");
+      // eslint-disable-next-line no-fallthrough
+      case "touchcancel":
+        console.log("touchcancel");
+        dispatch({
+          type: GridworldActionType.STOP_DRAWING,
+        });
+        break;
+    }
+  };
+
   return (
     <svg
       viewBox={`0 0 ${WIDTH} ${HEIGHT}`}
-      className="mx-auto max-h-[80vh]"
+      className="mx-auto max-h-[80vh] touch-none"
       onMouseDown={handleMouseEvent}
       onMouseMove={handleMouseEvent}
       onMouseUp={handleMouseEvent}
       onMouseLeave={handleMouseEvent}
+      onTouchStart={handleTouchEvent}
+      onTouchMove={handleTouchEvent}
+      onTouchEnd={handleTouchEvent}
+      onTouchCancel={handleTouchEvent}
     >
       {positions.map(([row, column]) => (
         <svg
