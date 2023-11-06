c9f741aa9b8f15a57d3c903ec5409d61a343aa8f
Thomas Saint-Gérand <devspaceship@gmail.com>
2023-11-06 19:23:19 +0100

    [gridworld] implement rust value iteration

--- gridworld/src/solvers.rs
+++ gridworld/src/solvers.rs
@@ -3,22 +3,28 @@ use crate::{
     utils::{matrix, policy_evaluation, policy_improvement},
 };
 
-pub fn policy_iteration(grid: Grid<Cell>, gamma: Option<f64>) -> (Grid<f64>, Grid<Policy>) {
+pub fn policy_value_iteration(
+    grid: Grid<Cell>,
+    gamma: Option<f64>,
+    iter_before_improvement: Option<u32>,
+) -> (Grid<f64>, Grid<Policy>) {
     let n = grid.len();
     let m = grid[0].len();
     let mut is_stable = false;
     let mut state_value_grid = matrix(n, m, 0.0);
     let mut policy_grid = matrix(n, m, Policy::Up);
     while !is_stable {
-        state_value_grid = policy_evaluation(&grid, &policy_grid, Some(&state_value_grid), gamma);
+        state_value_grid = policy_evaluation(
+            &grid,
+            &policy_grid,
+            Some(&state_value_grid),
+            gamma,
+            iter_before_improvement,
+        );
         is_stable = policy_improvement(&mut policy_grid, &grid, &state_value_grid, gamma);
     }
     (state_value_grid, policy_grid)
 }
-
-// fn value_iteration() {
-//     unimplemented!()
-// }
 
 // fn sarsa() {
 //     unimplemented!()
@@ -35,17 +41,17 @@ mod tests {
 
     #[test]
     fn test_policy_iteration() {
-        // Test policy_iteration function here
         let test_grid = get_test_grid();
-        let (_state_value_grid, policy_grid) = policy_iteration(test_grid, None);
+        let (_state_value_grid, policy_grid) = policy_value_iteration(test_grid, None, None);
         assert!(get_optimal_policy() == policy_grid);
     }
 
-    // #[test]
-    // fn test_value_iteration() {
-    //     // Test value_iteration function here
-    //     assert_eq!(2 + 2, 4);
-    // }
+    #[test]
+    fn test_value_iteration() {
+        let test_grid = get_test_grid();
+        let (_state_value_grid, policy_grid) = policy_value_iteration(test_grid, None, Some(5));
+        assert!(get_optimal_policy() == policy_grid);
+    }
 
     // #[test]
     // fn test_sarsa() {
