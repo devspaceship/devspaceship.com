0a34e05e61aec423ecbb0e95e97f1716ad1419c5
Thomas Saint-Gerand <devspaceship@gmail.com>
2023-12-18 10:05:55 +0100

    [madepro] augmented config

--- madepro/src/defaults.rs
+++ madepro/src/defaults.rs
@@ -4,3 +4,4 @@ pub const NUM_EPISODES: u32 = 500;
 pub const LEARNING_RATE: f64 = 0.3;
 pub const INITIAL_EXPLORATION_RATE: f64 = 1.0;
 pub const EXPLORATION_PERIOD: u32 = 300;
+pub const ITERATIONS_BEFORE_IMPROVEMENT: Option<u32> = None;
