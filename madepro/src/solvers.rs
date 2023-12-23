63793000fa0e1a8317b340615ecc122623c227a5
Thomas Saint-Gerand <devspaceship@gmail.com>
2023-12-23 07:41:57 +0100

    [madepro] change policy value iteration return type

--- madepro/src/solvers.rs
+++ madepro/src/solvers.rs
@@ -4,25 +4,30 @@ use crate::{
     utils::{infer_policy, policy_evaluation},
 };
 
-fn policy_value_iteration<M>(mdp: &M, config: &Config) -> Policy<M::State, M::Action>
+fn policy_value_iteration<M>(
+    mdp: &M,
+    config: &Config,
+) -> (StateValue<M::State>, Policy<M::State, M::Action>)
 where
     M: MDP,
 {
     let mut state_value = StateValue::new();
     let mut policy = Policy::new();
     loop {
-        let new_state_value = policy_evaluation(mdp, config, &policy, Some(state_value));
-        let new_policy = infer_policy(mdp, config, &new_state_value);
+        state_value = policy_evaluation(mdp, config, &policy, Some(state_value));
+        let new_policy = infer_policy(mdp, config, &state_value);
         if new_policy == policy {
             break;
         }
-        state_value = new_state_value;
         policy = new_policy;
     }
-    policy
+    (state_value, policy)
 }
 
-pub fn policy_iteration<M>(mdp: &M, config: &Config) -> Policy<M::State, M::Action>
+pub fn policy_iteration<M>(
+    mdp: &M,
+    config: &Config,
+) -> (StateValue<M::State>, Policy<M::State, M::Action>)
 where
     M: MDP,
 {
@@ -33,7 +38,10 @@ where
     policy_value_iteration(mdp, config)
 }
 
-pub fn value_iteration<M>(mdp: &M, config: &Config) -> Policy<M::State, M::Action>
+pub fn value_iteration<M>(
+    mdp: &M,
+    config: &Config,
+) -> (StateValue<M::State>, Policy<M::State, M::Action>)
 where
     M: MDP,
 {
