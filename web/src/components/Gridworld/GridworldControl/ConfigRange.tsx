1490caea56ab50feabf7235b6f9de3622954133e
Thomas Saint-Gérand <devspaceship@gmail.com>
2024-05-13 02:25:47 +0200

    [shadcn-ui] gridworld sliders

--- web/src/components/Gridworld/GridworldControl/ConfigRange.tsx
+++ web/src/components/Gridworld/GridworldControl/ConfigRange.tsx
@@ -5,6 +5,11 @@ import {
   GridworldStateContext,
 } from "../GridworldContextProvider";
 import { GridworldActionType } from "../actions";
+import { cn } from "@/lib/utils";
+import { Slider } from "@/components/ui/slider";
+import { Label } from "@/components/ui/label";
+
+type SliderProps = React.ComponentProps<typeof Slider>;
 
 const ConfigRange = ({
   id,
@@ -12,15 +17,13 @@ const ConfigRange = ({
   min,
   max,
   step,
-  isInteger = false,
   isLog = false,
 }: {
   id: string;
   label: string;
   min: number;
   max: number;
-  step: number | "any";
-  isInteger?: boolean;
+  step?: number;
   isLog?: boolean;
 }) => {
   const state = useContext(GridworldStateContext);
@@ -28,34 +31,30 @@ const ConfigRange = ({
   const configKey = id.replace(/-(\w)/g, (_, letter: string) =>
     letter.toUpperCase()
   );
-  const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
-    const value = isInteger
-      ? parseInt(event.target.value)
-      : parseFloat(event.target.value);
+
+  const handleChange = (value: number[]) => {
     dispatch({
       type: GridworldActionType.SET_CONFIG,
       config: {
         ...state.config,
-        [configKey]: value,
+        [configKey]: value[0],
       },
     });
   };
+
   const value = state.config[configKey as keyof GridworldConfig];
   return (
-    <div className="my-3">
-      <div>
-        <label htmlFor={id}>{label}</label>
-      </div>
-      <input
-        type="range"
+    <div className="m-3">
+      <Label htmlFor={id}>{label}</Label>
+      <Slider
         id={id}
         name={id}
         min={min}
         max={max}
         step={step}
-        value={value}
-        onChange={handleChange}
-        className="ml-2 mr-4"
+        value={[value]}
+        onValueChange={handleChange}
+        className="mx-auto my-2 w-32"
       />
       <div>{isLog ? `10^${value}` : value}</div>
     </div>
