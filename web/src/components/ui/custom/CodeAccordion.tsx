0da7b9f216fbc1777b88a9b0ef19fc51559e71d3
Thomas Saint-Gérand <devspaceship@gmail.com>
2024-05-29 00:05:24 +0200

    [gridworld] code accordion

---
+++ web/src/components/ui/custom/CodeAccordion.tsx
@@ -0,0 +1,26 @@
+import { ReactNode } from "react";
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "ui/accordion";
+
+const CodeAccordion = ({
+  children,
+  title = "See code",
+}: {
+  children: ReactNode;
+  title?: string;
+}) => {
+  return (
+    <Accordion type="single" collapsible>
+      <AccordionItem value="item-1">
+        <AccordionTrigger>{title}</AccordionTrigger>
+        <AccordionContent>{children}</AccordionContent>
+      </AccordionItem>
+    </Accordion>
+  );
+};
+
+export default CodeAccordion;
