a84ad9f7835d2c62e3c0e03a9c41e5fa5b6171da
Thomas Saint-Gérand <devspaceship@gmail.com>
2024-09-08 23:24:55 +0200

    [tanstack-query] dried queries

--- web/src/queries/elevator.tsx
+++ web/src/queries/elevator.tsx
@@ -12,10 +12,7 @@ export const useDensityData = () => {
   return useQuery({
     queryKey: ["densityData"],
     queryFn: async () => {
-      const response = await fetch("/static/posts/elevator/density.csv");
-      const text = await response.text();
-      const data = Papa.parse(text, { header: true, dynamicTyping: true });
-      const rows = data.data.slice(0, -1) as DataRow[];
+      const rows = await fetchParse("/static/posts/elevator/density.csv");
       for (const row of rows) {
         row.mass = Number(row.mass);
       }
@@ -28,10 +25,14 @@ export const useMotionData = () => {
   return useQuery({
     queryKey: ["motionData"],
     queryFn: async () => {
-      const response = await fetch("/static/posts/elevator/motion.csv");
-      const text = await response.text();
-      const data = Papa.parse(text, { header: true, dynamicTyping: true });
-      return data.data.slice(0, -1) as DataRow[];
+      return await fetchParse("/static/posts/elevator/motion.csv");
     },
   });
+};
+
+const fetchParse = async (url: string) => {
+  const response = await fetch(url);
+  const text = await response.text();
+  const data = Papa.parse(text, { header: true, dynamicTyping: true });
+  return data.data.slice(0, -1) as DataRow[];
 };
