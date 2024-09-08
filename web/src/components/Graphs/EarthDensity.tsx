9bd5b4a4ce3852e34f97ec59f3015c788251094a
Thomas Saint-Gérand <devspaceship@gmail.com>
2024-09-09 00:33:24 +0200

    [elevator] refactor graphs

--- web/src/components/Graphs/EarthDensity.tsx
+++ web/src/components/Graphs/EarthDensity.tsx
@@ -11,76 +11,27 @@ import {
 } from "recharts";
 import QueryClientProviderWrapper from "@/components/QueryClientProviderWrapper";
 import { useDensityData } from "@/queries/elevator";
+import { EarthDensityTooltip } from "./Tooltips";
 
 type EarthDensityProps = {
   dataKey: string;
 };
 
-const EarthDensityChart = (props: EarthDensityProps) => {
+const EarthDensityChart = ({ dataKey }: EarthDensityProps) => {
   const { isPending, error, data } = useDensityData();
 
   if (isPending) return "Loading...";
   if (error) return "An error has occurred while loading the data";
 
-  const CustomTooltip = ({
-    payload,
-    active,
-  }: {
-    payload?: {
-      payload: {
-        radius: number;
-        density: number;
-        mass: number;
-        gravity: number;
-      };
-    }[];
-    active?: unknown;
-  }) => {
-    if (!active || !payload) {
-      return null;
-    }
-
-    const earth_layers: [number, string][] = [
-      [6350, "Mantle"],
-      [3500, "Outer Core"],
-      [1200, "Inner Core"],
-    ];
-
-    const data = payload[0].payload;
-    const radius = Math.round(data.radius / 50) * 50;
-    const density = Math.round(data.density * 100) / 100;
-    const mass = Math.round(data.mass / 1e22) * 1e22;
-    const gravity = Math.round(data.gravity * 100) / 100;
-
-    let earth_layer = "Crust";
-    for (const [r, part] of earth_layers) {
-      if (radius < r) {
-        earth_layer = part;
-      }
-    }
-
-    return (
-      <div>
-        <p>{earth_layer}</p>
-        <p>{`radius: ${radius}km`}</p>
-        {props.dataKey == "density" ? <p>{`density: ${density}`}</p> : null}
-        {props.dataKey == "mass" ? <p>{`mass: ${mass}kg`}</p> : null}
-        {props.dataKey == "gravity" ? (
-          <p>{`gravity: ${gravity}m/(s^2)`}</p>
-        ) : null}
-      </div>
-    );
-  };
-
   return (
     <ResponsiveContainer width="95%" height={400}>
       <LineChart width={400} height={400} data={data}>
-        <Line type="monotone" dataKey={props.dataKey} stroke="#26c5de" />
+        <Line type="monotone" dataKey={dataKey} stroke="#26c5de" />
         <CartesianGrid stroke="#ccc" />
         <XAxis dataKey="radius" type="number" />
         <YAxis type="number" />
         <Tooltip
-          content={<CustomTooltip />}
+          content={<EarthDensityTooltip dataKey={dataKey} />}
           wrapperStyle={{ backgroundColor: "#222", padding: "8px" }}
         />
       </LineChart>
--- web/src/components/Graphs/Motion.tsx
+++ web/src/components/Graphs/Motion.tsx
@@ -10,60 +10,22 @@ import {
 } from "recharts";
 import QueryClientProviderWrapper from "@/components/QueryClientProviderWrapper";
 import { useMotionData } from "@/queries/elevator";
+import { MotionTooltip } from "./Tooltips";
 
 type MotionProps = {
   speed: boolean;
 };
 
-const MotionChart = (props: MotionProps) => {
+const MotionChart = ({ speed }: MotionProps) => {
   const { isPending, error, data } = useMotionData();
 
   if (isPending) return "Loading...";
   if (error) return "An error has occurred while loading the data";
 
-  const CustomTooltip = ({
-    payload,
-    active,
-  }: {
-    payload?: {
-      payload: {
-        radius: number;
-        time: number;
-        speed: number;
-        acceleration: number;
-      };
-    }[];
-    active?: unknown;
-  }) => {
-    if (!active || !payload) {
-      return null;
-    }
-
-    const data = payload[0].payload;
-    const time = Math.round(data.time / 10) * 10;
-    const radius = Math.round(data.radius / 50) * 50;
-    const speed = Math.round(data.speed * 10) / 10;
-    const acceleration = Math.round(data.acceleration * 10) / 10;
-
-    return (
-      <div>
-        <p>{`time: ${time}s`}</p>
-        {props.speed ? (
-          <>
-            <p>{`speed: ${speed}km/s`}</p>
-            <p>{`acceleration: ${acceleration}m/(s^2)`}</p>
-          </>
-        ) : (
-          <p>{`radius: ${radius}km`}</p>
-        )}
-      </div>
-    );
-  };
-
   return (
     <ResponsiveContainer width="95%" height={400}>
       <LineChart width={400} height={400} data={data}>
-        {props.speed ? (
+        {speed ? (
           <>
             <Line type="monotone" dataKey="speed" stroke="#c5de26" />
             <Line type="monotone" dataKey="acceleration" stroke="#26c5de" />
@@ -75,7 +37,7 @@ const MotionChart = (props: MotionProps) => {
         <XAxis dataKey="time" type="number" />
         <YAxis type="number" />
         <Tooltip
-          content={<CustomTooltip />}
+          content={<MotionTooltip speed={speed} />}
           wrapperStyle={{ backgroundColor: "#222", padding: "8px" }}
         />
       </LineChart>
