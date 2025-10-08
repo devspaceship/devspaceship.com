b40161a50da8286e7267cfd8dedc655bbb18d77b
Thomas Saint-Gérand <thomas@devspaceship.com>
2025-10-08 08:54:06 +0200

    [style] fix tailwind prose

--- web/src/app/posts/(posts)/layout.tsx
+++ web/src/app/posts/(posts)/layout.tsx
@@ -3,7 +3,24 @@ import "@highlightjs/cdn-assets/styles/github-dark.min.css";
 
 const PostLayout = ({ children }: { children: React.ReactNode }) => {
 	return (
-		<article className="container prose prose-invert mx-auto my-7 max-w-none p-3 prose-headings:text-primary prose-a:font-semibold prose-a:text-primary prose-a:no-underline hover:prose-a:text-primary hover:prose-a:underline prose-img:mx-auto">
+		<article
+			className="
+				container
+				prose
+				prose-invert
+				mx-auto
+				my-7
+				max-w-none
+				p-3
+				prose-headings:text-primary
+				prose-a:font-semibold
+				prose-a:text-primary
+				prose-a:no-underline
+				prose-a:hover:text-primary
+				prose-a:hover:underline
+				prose-img:mx-auto
+			"
+		>
 			{children}
 		</article>
 	);
